af33d6f1c470e6f6c731cc32d7fc5389f8f83348
meain <abinsimon10@gmail.com>
2016-05-20 11:42:12 +0530

    Right star color change

--- master.js
+++ master.js
@@ -1,3 +1,21 @@
+
+// For right star color change
+var timer
+function randomColor() {
+var color = '#' + Math.random().toString(16).substr(2, 6);
+document.getElementById("right-star").style.color = color;
+clearTimeout(timer);
+timer=setTimeout(clearColor,50);
+console.log(color);
+}
+function clearColor() {
+document.getElementById("right-star").style.color = "grey";
+}
+document.addEventListener('mousemove', randomColor);
+
+document.addEventListener('touchmove', randomColor);
+
+// Particle
 var Particle, addParticle, animationLoop, backingStoreRatio, canvas, context, destroyParticlesOutsideCanvasBounds, devicePixelRatio, maxVelocity, oldHeight, oldWidth, particlesArray, particlesIds, particlesOrigin, random, randomColor, randomInteger, ratio, updateParticlesOrigin;
 
 canvas = document.getElementById('dots');
